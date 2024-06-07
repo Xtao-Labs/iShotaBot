148a393068fecedd29a2d12fce01d5bf491b41a4
xtaodada <xtao@xtaolink.cn>
2024-06-07 23:38:42 +0800

    fix: temp fix

--- models/temp_fix.py
+++ models/temp_fix.py
@@ -6,11 +6,14 @@ async def temp_fix(
     message: pyrogram.raw.base.Message,
     users: dict,
     chats: dict,
+    topics: dict = None,
     is_scheduled: bool = False,
     replies: int = 1,
+    business_connection_id: str = None,
+    reply_to_message: "raw.base.Message" = None
 ):
     parsed = await pyrogram.types.Message.old_parse(
-        client, message, users, chats, is_scheduled, replies
+        client, message, users, chats, topics, is_scheduled, replies, business_connection_id, reply_to_message
     )  # noqa
     if (
         isinstance(message, pyrogram.raw.types.Message)
